504784525fbdfdf794fdef80f57db2423923f9c4
Chen Jiaxin <chenjiaxin@shopee.com>
2026-05-30 00:59:30 +0800

    fake lib

--- src/fakeinputlibs/linux/input-event-codes.h
+++ src/fakeinputlibs/linux/input-event-codes.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <linux/input.h>
+
+#ifndef BTN_LEFT
 #define BTN_LEFT 0x110
+#endif
+#ifndef BTN_RIGHT
 #define BTN_RIGHT 0x111
+#endif
+#ifndef BTN_MIDDLE
 #define BTN_MIDDLE 0x112
+#endif
+#ifndef BTN_TASK
 #define BTN_TASK 0x117
+#endif
